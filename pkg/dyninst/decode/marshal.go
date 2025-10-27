3064a63d4d62bf49d8ffbc0ca267d601133ba5f7
grantseltzer <grantseltzer@gmail.com>
2025-10-27 11:50:53 -0500

    Changes to expression generate for template segments

--- pkg/dyninst/decode/marshal.go
+++ pkg/dyninst/decode/marshal.go
@@ -216,7 +216,6 @@ func (ce *captureEvent) MarshalJSONTo(enc *jsontext.Encoder) error {
 		return errors.New("presence bitset is out of bounds")
 	}
 	presenceBitSet := ce.rootData[:ce.rootType.PresenceBitsetSize]
-
 	if err := writeTokens(enc, jsontext.BeginObject); err != nil {
 		return err
 	}
